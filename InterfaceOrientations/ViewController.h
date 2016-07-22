1b2283bee5afcd826ad17647b1e1a5f36f50eedc
qfyige <qfyige@163.com>
2016-07-22 10:30:30 +0800

    Initial Commit

---
+++ InterfaceOrientations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterfaceOrientations
+//
+//  Created by tong li on 16/7/22.
+//  Copyright © 2016年 tong li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
